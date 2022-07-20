c2751886170f5942dd8bada9228346c17d9a7c5f
RakeshAhire <rocky5509ahire@gmail.com>
2022-07-20 23:45:46 +0530

    day-2 work added

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// let firstslide=[
+//     "https://www.bigbasket.com/media/uploads/banner_images/all_hp_m_bcd_paneer_1600x460_250622.jpg",
+//     "https://www.bigbasket.com/media/uploads/banner_images/hp_fom_m_bbpl-staples_460_180722_Bangalore.jpg",
+//     "https://www.bigbasket.com/media/uploads/banner_images/hp_m_Dry_FishBanner_1600x460_200722.jpg",
+//     "https://www.bigbasket.com/media/uploads/banner_images/fresho_days_fnv_Bangalore-1600x460_19thJULY22.jpeg",
+//     "https://www.bigbasket.com/media/uploads/banner_images/t1_hp_sbf_m_snacking-store_1600x460_250622.jpg",
+//     "https://www.bigbasket.com/media/uploads/banner_images/all_hp_m_cmc_happychef_1600x460_250622.jpg",
+// ];
+
