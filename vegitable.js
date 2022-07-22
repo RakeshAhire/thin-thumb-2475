50e1b191a147af5a624674eab7c8b9b76f9863c3
RakeshAhire <rocky5509ahire@gmail.com>
2022-07-23 01:10:55 +0530

    product data add

---
+++ vegitable.js
@@ -0,0 +1,309 @@
+let prodData = [
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 20,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 45,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000150_19-fresho-onion.jpg",
+        productName: "Onion",
+        productprice: 32.32
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 50,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 45,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000201_15-fresho-tomato-hybrid.jpg",
+        productName: "Tomato Hybrid",
+        productprice: 36
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 51,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 50.2,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000074_19-fresho-cauliflower.jpg",
+        productName: "Cauliflower/Phul Kobi",
+        productprice: 45
+    },
+
+    {
+        Brand: "BB-Combo",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 65,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 40,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000143_14-fresho-ladies-finger.jpg",
+        productName: "Ladies Finger/Bhendi",
+        productprice: 21.21
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 25,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 57.5,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000273_16-fresho-mushrooms-button.jpg",
+        productName: "Mushrooms",
+        productprice: 39.39
+    },
+    {
+        Brand: "BB-Combo",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 21,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 113,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000069_20-fresho-capsicum-green.jpg",
+        productName: "Capsicum/Dhobli Mirchi",
+        productprice: 68.68
+    },
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 54,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 65,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000103_16-fresho-cucumber.jpg",
+        productName: "Kakdi- Hirvi",
+        productprice: 44.44
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 34,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 112,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000031_21-fresho-banana-yelakki.jpg",
+        productName: "Banana/Kele",
+        productprice: 90
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 35,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 140,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000271_13-fresho-carrot-ooty.jpg",
+        productName: "Carrot/Gajar",
+        productprice: 119
+    },
+
+   {
+        Brand: "BB-Combo",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 45,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 112,
+        image: "https://www.bigbasket.com/media/uploads/p/s/50000526_6-fresho-lemon-organically-grown.jpg",
+        productName: "Lemon-Organically",
+        productprice: 90
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 54,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 77.56,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000047_21-fresho-beetroot.jpg",
+        productName: "Beetroot/Beet",
+        productprice: 56
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 34,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 51,
+        image: "https://www.bigbasket.com/media/uploads/p/s/40057966_2-fresho-tender-coconut-medium.jpg",
+        productName: "Tender Coconut"
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 45,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 31,
+        image: "https://www.bigbasket.com/media/uploads/p/s/40048457_7-fresho-potato-new-crop.jpg",
+        productName: "Potato-New Crop",
+        productprice: 25
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 45,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 223.19,
+        image: "https://www.bigbasket.com/media/uploads/p/s/40226802_1-fresho-baby-apple-shimla.jpg",
+        productName: "Baby Apple",
+        productprice: 179
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 34,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 5.5,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000097_19-fresho-coriander-leaves.jpg",
+        productName: "Coriander/Sambhar",
+        productprice: 4.4
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 54,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 83.75,
+        image: "https://www.bigbasket.com/media/uploads/p/s/10000044_42-fresho-beans-french-ring.jpg",
+        productName: "Beans-French,Ring",
+        productprice: 50.5
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 34,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 95.47,
+        image: "https://www.bigbasket.com/media/uploads/p/s/40029980_3-fresho-ginger-organically-grown.jpg",
+        productName: "Ginger",
+        productprice: 50.5
+    },
+
+    {
+        Brand: "Fresho",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 34,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 171,
+        image: "https://www.bigbasket.com/media/uploads/p/mm/30000645_12-fresho-chilli-hybrid.jpg",
+        productName: "Chilli-Hybrid",
+        productprice: 137.15
+    },
+
+    {
+        Brand: "BB-Combo",
+        Date: "Standard Delivery : 10AM To 6PM",
+        OFF: 54,
+        QtyPiece: "Price of 1 Kg",
+        QtyPieceForOneElement: "1 Kg",
+        StrikePrice: 61.21,
+        image: "https://www.bigbasket.com/media/uploads/p/mm/10000166_16-fresho-radish-white.jpg",
+        productName: "Radish White",
+        productprice: 36.61
+    }
+]
+
+display(prodData)
+function display(prodData){
+    console.log("abc")
+    prodData.forEach(function(ele){
+        let maindiv=document.createElement("div");
+        maindiv.setAttribute("class", "maindiv");
+        
+        let offerdiv=document.createElement("div");
+        offerdiv.innerText = "Get"+" "+ele.OFF+" "+"off";
+         offerdiv.setAttribute("class", "offer");
+
+        let mainimg=document.createElement("img");
+         mainimg.src = ele.image;
+         mainimg.setAttribute("class", "mainimg");
+
+        let namediv=document.createElement("div");
+        let childp1=document.createElement("p");
+        childp1.innerText = ele.Brand;
+        let childp2=document.createElement("p");
+        childp2.innerText = ele.productName;
+
+        namediv.append(childp1,childp2)
+
+        let select=document.createElement("select");
+        select.value = ele.QtyPiece;
+        select.setAttribute("class", "select");
+
+
+        let truckdiv=document.createElement("div");
+        
+        let pricediv=document.createElement("div");
+        pricediv.setAttribute("class", "pricediv");
+        let mrp=document.createElement("p");
+        mrp.innerText="MRP:"+ele.productprice +"/kg";
+        mrp.style.marginRight="10px";
+        
+        
+
+        let cuttp=document.createElement("del");
+        cuttp.innerText=ele.StrikePrice;
+        cuttp.style.fontSize="13px";
+        pricediv.append(mrp,cuttp)
+
+        let del_div=document.createElement("div");
+        del_div.setAttribute("class", "del_div");
+        let trimg=document.createElement("img");
+         trimg.src="./imgs/delivery-truck.png";
+         trimg.style.width="20%";
+        let del_p=document.createElement("p");
+        del_p.innerText=ele.Date;
+
+        del_div.append(trimg,del_p);
+
+        let inputdiv=document.createElement("div");
+        inputdiv.setAttribute("id","last") ;
+        let qtydiv=document.createElement("div");
+        qtydiv.innerText = "Qty" ;
+        qtydiv.setAttribute("class","Qty");
+
+        let quantity=document.createElement("input");
+        quantity.type="number";
+        quantity.setAttribute("class","quantity");
+        
+
+        let cartbtn=document.createElement("div");
+        cartbtn.setAttribute("class", "cartbtn");
+        let addp=document.createElement("p");
+        addp.innerText="ADD";
+        let logoimg=document.createElement("img");
+        logoimg.src="https://cdn-icons-png.flaticon.com/128/117/117641.png";
+        logoimg.style.width="30%";
+
+        cartbtn.append(addp,logoimg);
+        inputdiv.append(qtydiv,quantity,cartbtn);
+
+        truckdiv.append(pricediv,del_div,inputdiv);
+        maindiv.append(offerdiv,mainimg,namediv,select,truckdiv);
+
+        document.querySelector(".container").append(maindiv);
+
+    });
+}
